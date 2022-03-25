8d96a22012107eaed2c50df4d309666606eaddee
lucyrandrews <lucyrandrews@gmail.com>
2022-03-25 14:40:44 -0700

    initial set cover attempt - pushing in case things crash

--- scripts/03_analyze_networks/04_run_set_cover.R
+++ scripts/03_analyze_networks/04_run_set_cover.R
@@ -7,3 +7,18 @@
 
 # prep data structure for weighted set cover with function `weightedSetCover`
 # from `WebGestaltR` package
+
+# create list of ids contained in each set
+ids_in_set <- sapply(set_costs$comids_str_concat, strsplit, split = " ")
+
+# name each set with the gaged comids
+names(ids_in_set) <- set_costs$gage_comid
+
+
+
+# Run the greedy set cover analysis with weights (aka costs - inverted values)
+# to preferentially select "high value" gage sites
+set_cover_output <- weightedSetCover(idsInSet = ids_in_set,
+                                     costs = set_costs$set_cost_rescaled,
+                                     topN = nrow(set_costs))
+
