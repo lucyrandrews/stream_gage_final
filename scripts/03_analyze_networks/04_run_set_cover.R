a8a41e91f98d2391566b9d834a611c8609c6c789
lucyrandrews <lucyrandrews@gmail.com>
2022-03-27 21:25:45 -0700

    set cover code + flow - options for weighted and unweighted, weighted currently unfinished cuz buggy

--- scripts/03_analyze_networks/04_run_set_cover.R
+++ scripts/03_analyze_networks/04_run_set_cover.R
@@ -3,22 +3,44 @@
 # This script runs a set-cover analysis to identify the compete set of gages
 # that most efficiently covers the entires stream network.
 
-# Structure data to use in set cover analysis ----
+# Run set cover analysis ----
 
-# prep data structure for weighted set cover with function `weightedSetCover`
-# from `WebGestaltR` package
-
-# create list of ids contained in each set
-ids_in_set <- sapply(set_costs$comids_str_concat, strsplit, split = " ")
-
-# name each set with the gaged comids
-names(ids_in_set) <- set_costs$gage_comid
-
-
-
-# Run the greedy set cover analysis with weights (aka costs - inverted values)
-# to preferentially select "high value" gage sites
-set_cover_output <- weightedSetCover(idsInSet = ids_in_set,
-                                     costs = set_costs$set_cost_rescaled,
-                                     topN = nrow(set_costs))
-
+if(unweighted_set_cover) {
+  
+  # prep set cover input dataframe
+  set_cover_input <- network_analysis_long %>%
+    select(gage_comid, comid) %>%
+    rename(set = gage_comid,
+           element = comid)
+  
+  # run set cover and clean output
+  set_cover_output <- greedySetCover(set_cover_input, data.table = TRUE) %>%
+    rename(gage_comid = set,
+           comid = element)
+  
+  # clean up
+  rm(set_cover_input)
+  
+} else {
+  
+  # create list of ids contained in each set
+  sets_w_elements <- sapply(set_costs$comids_str_concat, strsplit, split = " ")
+  
+  # name each set with the gaged comids
+  names(sets_w_elements) <- set_costs$gage_comid
+  
+  # create costs vector
+  cost_weights <- as.vector(set_costs$set_cost_rescaled)
+  
+  # name the costs
+  names(cost_weights) <- set_costs$gage_comid
+  
+  # Run the greedy set cover analysis with weights (aka costs - inverted values)
+  # to preferentially select "high value" gage sites
+  # See draft functions for what is in progress here
+  set_cover_output <- NULL
+  
+  # clean up
+  rm(sets_w_elements, cost_weights)
+  
+}
